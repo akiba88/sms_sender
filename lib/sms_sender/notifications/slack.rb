970445e3c1a59eb569050146d7d7d836c0d08b20
Anton Kiba <akiba788@mail.ru>
2016-07-29 15:43:47 +0300

    fix providers & notifications

--- lib/sms_sender/notifications/slack.rb
+++ lib/sms_sender/notifications/slack.rb
@@ -1,55 +1,57 @@
 require 'slack-notifier'
 
-class Notifications::Slack
-  attr_reader :object, :notifier
+module Notifications
+  class Slack
+    attr_reader :object, :notifier
 
-  def initialize(object)
-    @object = object
-    @notifier = Slack::Notifier.new(options[:path], channel: options[:channel], username: options[:username])
-  end
+    def initialize(object)
+      @object = object
+      @notifier = Slack::Notifier.new(options[:path], channel: options[:channel], username: options[:username])
+    end
 
-  def run(title, fallback, type)
-    notifier.ping notifier_params(title, fallback, type)
-  end
+    def run(title, fallback, type)
+      notifier.ping notifier_params(title, fallback, type)
+    end
 
-protected
+  protected
 
-  def options
-    @options ||= SmsSender.config.notifications_options[:slack]
-  end
+    def options
+      @options ||= SmsSender.config.notifications_options[:slack]
+    end
 
-  def notifier_params(title, fallback, type)
-    {
-      attachments: [
-        {
-          color: 'danger',
-          title: title,
-          title_link: options[:callback_url],
-          fallback: fallback,
-          fields: [
-            {
-              title: 'Type',
-              value: type,
-              short: true
-            },
-            {
-              title: 'Environment',
-              value: SmsSender.config.environment,
-              short: true
-            },
-            {
-              title: 'Project',
-              value: SmsSender.config.project,
-              short: true
-            },
-            {
-              title: object.class.name,
-              value: object.id,
-              short: true
-            }
-          ]
-        }
-      ]
-    }
+    def notifier_params(title, fallback, type)
+      {
+        attachments: [
+          {
+            color: 'danger',
+            title: title,
+            title_link: options[:callback_url],
+            fallback: fallback,
+            fields: [
+              {
+                title: 'Type',
+                value: type,
+                short: true
+              },
+              {
+                title: 'Environment',
+                value: SmsSender.config.environment,
+                short: true
+              },
+              {
+                title: 'Project',
+                value: SmsSender.config.project,
+                short: true
+              },
+              {
+                title: object.class.name,
+                value: object.id,
+                short: true
+              }
+            ]
+          }
+        ]
+      }
+    end
   end
 end
