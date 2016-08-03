e424dd83c586044bd9510a13d4aebb356a637e7d
Anton Kiba <akiba788@mail.ru>
2016-08-03 11:48:56 +0300

    fix call sender process

--- lib/sms_sender/workers/process_worker.rb
+++ lib/sms_sender/workers/process_worker.rb
@@ -10,7 +10,7 @@ module SmsSender
       def perform(object_class, object_id, args)
         object = Object.const_get(object_class).find(object_id)
 
-        SmsSender::Process.new(args[:channel]).run(object, args[:message], args[:phone_number])
+        SmsSender::Process.new(args['channel']).run(object, args['message'], args['phone_number'])
       end
     end
   end
